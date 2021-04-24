07fba89b01396db8f9ec9a21a247fb391b20feec
HarperT <harper.thurlow@my.jcu.edu.au>
2021-04-24 14:18:48 +1000

    create valid login system that checks blank username.

--- login.php
+++ login.php
@@ -1,7 +1,30 @@
 <?php
 error_reporting(E_ALL);
-session_start();
-$_SESSION['username'] = $_REQUEST['username'];
+function make_error_message($letters)
+{
+
+    $phrase = "";
+    foreach ($letters as $letter) {
+        $phrase .= $letter;
+    }
+    $phrase .= " enter a username";
+    echo $phrase;
+}
+
+if ($_REQUEST['username'] != null){
+    session_start();
+    $_SESSION['username'] = $_REQUEST['username'];
 //echo $_GET['page'];
-header("Location: ".$_GET['page'].".php");
+    header("Location: ".$_GET['page'].".php");
+}
+
+else {
+//    header("Location: ".$_GET['page'].".php");
+    $letters = ["P", "l", "e", "a", "s", "e"];
+    make_error_message($letters);
+
+
+}
+
+
 exit();
